469cce7417a4062a3f853876772d1cc61024970c
Mark Leighton Fisher <markleightonfisher@gmail.com>
2013-11-18 17:27:42 -0500

    Perl::Critic fixes
    1) Various Perl::Critic fixes.
    2) Fixed the name of the distribution in dist.ini.

--- lib/Set/Jaccard/SimilarityCoefficient.pm
+++ lib/Set/Jaccard/SimilarityCoefficient.pm
@@ -1,31 +1,27 @@
 # -*- mode: cperl; -*-
 package Set::Jaccard::SimilarityCoefficient;
-
-# VERSION
-
-# ------ pragmas
-use utf8;
-use autodie;
 use warnings;
 use strict;
+use utf8;
+use autodie;
 use Exception::Class qw(
-    BadArgumentException
-    DivideByZeroException
+  BadArgumentException
+  DivideByZeroException
 );
 use Readonly qw( Scalar );
 use Set::Scalar;
 
-#
+# VERSION
 
+## no critic( Subroutines::ProhibitCallsToUnexportedSubs )
 # ------ Error messages
-Readonly::Scalar my $BAD_SET_A
-    => 'must have either ArrayRef or Set::Scalar value for set A';
-Readonly::Scalar my $BAD_SET_B
-    => 'must have either ArrayRef or Set::Scalar value for set B';
-Readonly::Scalar my $DIVIDE_BY_ZERO
-    => 'Cannot calculate when size(Union(A B)) == 0';
-
-#
+Readonly::Scalar my $BAD_SET_A =>
+  'must have either ArrayRef or Set::Scalar value for set A';
+Readonly::Scalar my $BAD_SET_B =>
+  'must have either ArrayRef or Set::Scalar value for set B';
+Readonly::Scalar my $DIVIDE_BY_ZERO =>
+  'Cannot calculate when size(Union(A B)) == 0';
+## use critic
 
 =function
 
@@ -34,38 +30,44 @@ Calculate the Jaccard Similarity Coefficient.
 =cut
 
 sub calc {
-    my ($set_a_arg, $set_b_arg) = @_;
+    my ( $set_a_arg, $set_b_arg ) = @_;
     my $set_a;
     my $set_b;
 
-    if (!defined $set_a_arg
-    || (ref $set_a_arg ne 'ARRAY' && ref $set_a_arg ne 'Set::Scalar')) {
+## no critic( Modules::RequireExplicitInclusion )
+    if ( !defined $set_a_arg
+        || ( ref $set_a_arg ne 'ARRAY' && ref $set_a_arg ne 'Set::Scalar' ) )
+    {
         BadArgumentException->throw($BAD_SET_A);
     }
 
-    if (!defined $set_b_arg
-    || (ref $set_b_arg ne 'ARRAY' && ref $set_b_arg ne 'Set::Scalar')) {
+    if ( !defined $set_b_arg
+        || ( ref $set_b_arg ne 'ARRAY' && ref $set_b_arg ne 'Set::Scalar' ) )
+    {
         BadArgumentException->throw($BAD_SET_B);
     }
 
-    if (ref $set_a_arg eq 'Set::Scalar') {
+    if ( ref $set_a_arg eq 'Set::Scalar' ) {
         $set_a = $set_a_arg->clone();
-    } else {
-        $set_a = Set::Scalar->new(@{$set_a_arg});
+    }
+    else {
+        $set_a = Set::Scalar->new( @{$set_a_arg} );
     }
 
-    if (ref $set_b_arg eq 'Set::Scalar') {
+    if ( ref $set_b_arg eq 'Set::Scalar' ) {
         $set_b = $set_b_arg->clone();
-    } else {
-        $set_b = Set::Scalar->new(@{$set_b_arg});
+    }
+    else {
+        $set_b = Set::Scalar->new( @{$set_b_arg} );
     }
 
     my $intersection = $set_a->intersection($set_b);
-    my $union        = $set_a->union(       $set_b);
+    my $union        = $set_a->union($set_b);
 
-    if ($union->size <= 0) {
+    if ( $union->size <= 0 ) {
         DivideByZeroException->throw($DIVIDE_BY_ZERO);
     }
+## use critic
 
     return $intersection->size / $union->size;
 }
